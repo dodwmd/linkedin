ab6cbcfdceaf274786036b7df093f68cd5a0abd1
Michael <1372930+dodwmd@users.noreply.github.com>
2024-09-07 13:25:18 +1000

    Set the default fixture loop scope

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -6,6 +6,13 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 
 from web_app import create_app
 
+# Set the default fixture loop scope
+@pytest.fixture(scope="function", autouse=True)
+def event_loop(request):
+    """Create an instance of the default event loop for each test case."""
+    loop = asyncio.get_event_loop_policy().new_event_loop()
+    yield loop
+    loop.close()
 
 @pytest.fixture
 def app():
@@ -15,7 +22,6 @@ def app():
     with patch('nats_manager.NatsManager', MockNatsManager), \
          patch('mysql_manager.MySQLManager', MockMySQLManager):
         yield app
-
 
 @pytest.fixture
 def client(app):
